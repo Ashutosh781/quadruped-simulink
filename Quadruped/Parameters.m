b0f6168bf0d4c9068cb08a095a67e870c5b3b1e2
Ashutosh Gupta <ashutosh.gupta.070801@gmail.com>
2020-07-20 05:19:00 +0530

    Corrected frames in model

--- Quadruped/Parameters.m
+++ Quadruped/Parameters.m
@@ -24,8 +24,11 @@ gait_time = 0.8; %In sec
 %Motor Targets
 %In deg
 target1 = 0;
-target2 = -120;
-target3 = 120;
+target2 = 30;
+target2r = -30;
+target3 = -120;
+target3r = 120;
+buffer = 8000;
 
 %%
 %Contact
@@ -38,6 +41,5 @@ critical_vel = 1e-3; %m/s
 
 %%
 %COM Height
-target2_orig = target2 + 90;
-h = l3_orig + l2 * sind(target2_orig) + 0.05;
+h = l3_orig + l2 * sind(target2r) + 0.05;
 %h = 0.25;

419313218dbc6fda26af2f0136015d3f977d96c9
Ashutosh Gupta <59316418+Ashutosh781@users.noreply.github.com>
2021-01-01 15:33:43 +0530

    ZMP almost completed

--- Quadruped/Ik_analytical.m
+++ Quadruped/Ik_analytical.m
@@ -1,9 +1,14 @@
+% [x,y,z] home values for test, in hip frame
 x = 0.54641;
 y = 0;
 z = 0.18;
-c = sqrt((x-l1*cos(t1))^2 + z^2);
+c = sqrt((x-l1)^2 + z^2);
 t3 = acosd((l2^2 + l3^2 - c^2)/(2*l2*l3));
 beta = acosd((c^2 + l2^2 - l3^2)/(2*l2*c));
-t2 = atand(((c*sind(beta)-l3*sind(t3))/(l3*cosd(t3)+c*cosd(beta)-l2)));
+t2 = atand(((c*sind(beta)-l3*sind(t3))/(l3*cosd(t3)+c*cosd(beta)-l2)))+ brute_error;
 t1 = atand(y/(l1+l2*cos(t2)+l3*cos(t3)));
 
+% t1 range [-90,90] here also true in atand and in actual bot never to violate this range
+% t2 range [-90,90] here also true in atand and in actual bot never to violate this range
+% t3 range [0,180] here true in acosd and in actual bot for right leg true, for left leg [-180,0]
+% t3 range can be increased to [-180,180] for giving capability for legs to do task
--- Quadruped/Parameters.m
+++ Quadruped/Parameters.m
@@ -20,7 +20,7 @@ spring_stiff = 1; %In N*m/rad
 damping = 0.01; %In N*m/(rad/s)
 
 %% Need to adjust and optimize these
-gait_time = 0.8; %In sec
+gait_time = 1; %In sec
 step_time = gait_time/2;
 step_height = 0.1; %In m
 
@@ -32,6 +32,7 @@ target2r = -30;
 target3 = -120;
 target3r = 120;
 buffer = 8000;
+brute_error = 3;
 
 %% Contact
 
@@ -47,40 +48,4 @@ h = l3_orig + l2 * sind(target2r) + 0.05;
 
 %% Predifened & Common
 g = 9.8066;
-iden3 = eye(3);
-E = zeros(6,6);
-E(1:3,1:3) = iden3;
-p = [0;0;1;0;0;0];
-
-%% IGNORE FOR NOW THE BELOW PART
-%% Module 0 = Body
-
-% N0 = P0 = [L0 0; 0 1] (6x6) Find L0 (3x3)
-rho = zeros(6,1);
-rho(6,1) = g;
-% M0 = [I0 mkdk x 1; -mkdk x 1 mk1] (6x6)
-% Omega0 = [wk x1 0; 0 wk x1] (6x6) wk = angular velocity
-% W0f = external wrench
-% W0 = final wrench
-
-%% Module 1 = RF leg
-
-% A10 = doubt
-b2_1 = 0;
-a2_1 = l1;
-alpha2_1 = -90;
-a21x1_1 = [0 b2_1*cosd(alpha2_1) b2_1*sind(alpha2_1); -b2_1*cosd(alpha2_1) 0 a2_1; -b2_1*sind(alpha2_1) -a2_1 0];
-
-A21_1 = eye(6);
-A21_1(4:6,1:3) = a21x1_1;
-% A21_1dot = doubt
-% Omega2_1 = doubt
-% M2_1 = doubt
-
-b3_1 = 0;
-a3_1 = l2;
-alpha3_1 = 0;
-a32x1_1 = [0 b3_1*cosd(alpha3_1) b3_1*sind(alpha3_1); -b3_1*cosd(alpha3_1) 0 a3_1; -b3_1*sind(alpha3_1) -a3_1 0];
-
-A32_1 = eye(6);
-A32_1(4:6,1:3) = a32x1_1;
+Xc_i = 0;
